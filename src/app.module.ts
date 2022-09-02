d29db7850fdd26ebc74a68654b430d37fa93e406
Wagner <wmaluk@gmail.com>
2022-09-02 18:03:45 -0300

    adicionado strategia local e jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UsersModule],
 })
 export class AppModule {}
